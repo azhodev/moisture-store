93abba98a0dc4cf3c04cb7405df36684ee43c502
Avaz Hojakeldiyev <azho.dev@gmail.com>
2025-07-06 01:06:11 +0500

    feat(ui): add Tailwind 4.1 and shadcn

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,18 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
   compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint']
+  modules: ['@nuxt/eslint', 'shadcn-nuxt'],
+  css: ['~/assets/css/tailwind.css'],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
+  },
+  shadcn: {
+    prefix: 'Ui',
+    componentDir: './components/ui'
+  },
 })
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+        <h1 class="text-amber-500 bg-amber-400">main</h1>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+
+</style>
